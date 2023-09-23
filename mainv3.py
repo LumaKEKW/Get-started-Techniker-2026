a133aab363918e12c5ea6bf78fa06ca4712a87b1
Luma <98356273+LumaKEKW@users.noreply.github.com>
2023-09-23 17:20:14 +0000

    Zahl2 != 0 , clean Code

--- mainv3.py
+++ mainv3.py
@@ -1,17 +1,20 @@
 while True:
     zahl1 = int(input('Taschenrechner:\nBitte erste Zahl eingeben:'))
     zahl2 = int(input('Bitte zweite Zahl eingeben:'))
-    rz = input('Und das Rechenzeichen eingeben: / oder * oder + oder -:')
-    match rz:
-        case '+':
-            erg1 = zahl1+zahl2
-            print ('Das Ergebnis der Addition lautet:', erg1)
-        case '-':
-            erg2 = zahl1-zahl2
-            print ('Das Ergebnis der Subtraktion lautet:', erg2)
-        case '/':
-            erg3 = zahl1/zahl2
-            print ('Das Ergebnis der Division lautet:', erg3)
-        case '*':
-            erg4 = zahl1*zahl2
-            print ('Das Ergebis der Multiplikation lautet:', erg4)
+    if zahl2 == 0:
+        print ('0 ist eine unzulässige Zahl!')
+    else:
+        rz = input('Und das Rechenzeichen eingeben: / oder * oder + oder -:')
+        match rz:
+            case '+':
+                erg1 = zahl1+zahl2
+                print ('Das Ergebnis der Addition lautet:', erg1)
+            case '-':
+                erg2 = zahl1-zahl2
+                print ('Das Ergebnis der Subtraktion lautet:', erg2)
+            case '/':
+                erg3 = zahl1/zahl2
+                print ('Das Ergebnis der Division lautet:', erg3)
+            case '*':
+                erg4 = zahl1*zahl2
+                print ('Das Ergebis der Multiplikation lautet:', erg4)
